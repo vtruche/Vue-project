74662e040125b0650f545c543886ca63e4a053cb
Vincent Truche <vince.truche@gmail.com>
2022-06-02 11:31:11 +0200

    OK : Exo compteur

---
+++ vue-project/src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { ref } from "vue";
+
+const compteur = ref(0);
+
+setInterval(() => {
+  compteur.value++
+}, 1000 )
+
+function resetCompteur(){
+  compteur.value = 0;
+}
+
+</script>
+
+<template>
+  <h1>Hello World</h1>
+  <p>Compteur : {{compteur}}</p>
+  <button v-on:click="resetCompteur()">Reset compteur</button>
+  <input type="number" v-model="compteur">
+</template>
+
+<style></style>
